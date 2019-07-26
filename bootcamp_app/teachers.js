fda5044fb2570aa0fba1aa8da7d8752599546e94
Simon Garber <Simon.garber@gmail.com>
2019-07-26 00:30:43 -0400

    working on parameterized queries for students and teachers js files

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -1,4 +1,16 @@
 const { Pool } = require('pg');
+ 
+const queryString = `select distinct teachers.name as teacher, cohorts.name as cohort
+from teachers
+join assistance_requests on teacher_id = teachers.id
+join students on student_id = students.id 
+join cohorts on cohort_id = cohorts.id
+where cohorts.name = '${process.argv[2] || 'JUL02'}' 
+order by teacher;`
+
+const cohortName = process.argv[2];
+const limit = process.argv[3]|| 5;
+const values = [`%${cohortName}%`, limit];
 
 const pool = new Pool({
     user: 'vagrant',
@@ -7,17 +19,10 @@ const pool = new Pool({
     database: 'bootcampx'
 });
 
-pool.query(`
-select distinct teachers.name as teacher, cohorts.name as cohort
-from teachers
-join assistance_requests on teacher_id = teachers.id
-join students on student_id = students.id 
-join cohorts on cohort_id = cohorts.id
-where cohorts.name = '${process.argv[2] || 'JUL02'}' 
-order by teacher; 
-`)
+pool.query(queryString,values)
 .then(res => {
     res.rows.forEach(row => {
         console.log(`${row.cohort}: ${row.teacher}`);
     })
-});
+})
+.catch(err => console.error(`query error`, err.stack));
